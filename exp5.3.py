3ce9f7bb9059fbeb8157fedf458b61303a0a1396
shreyajadhav8375-hub <shreyajadhav8375@gmail.com>
2026-03-17 13:34:30 +0530

    Add files via upload

---
+++ exp5.3.py
@@ -0,0 +1,13 @@
+
+
+"""
+Created on Tue Mar 17 07:36:08 2026
+
+@author:shreya
+"""
+books = {"Math": 300, "Physics": 400, "Chemistry": 350}
+
+# Update price
+books["Physics"] = 450
+
+print(books)
