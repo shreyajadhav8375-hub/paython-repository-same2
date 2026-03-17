e9aa96d11e85db1b026f42020edd1c52c794838b
shreyajadhav8375-hub <shreyajadhav8375@gmail.com>
2026-03-17 13:34:47 +0530

    Add files via upload

---
+++ exp5.4.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 07:37:33 2026
+
+@author: User
+"""
+classA = {1, 2, 3, 4, 5}
+classB = {4, 5, 6, 7}
+
+# Common students
+common = classA.intersection(classB)
+
+print(common)
