c9d473988fdc6b595e9d06edf3e3851cee495df8
shreyajadhav8375-hub <shreyajadhav8375@gmail.com>
2026-03-17 13:35:47 +0530

    Add files via upload

---
+++ exp5.7.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 07:41:46 2026
+
+@author: shreya
+"""
+
+account1 = {"rahul", "sneha", "amit", "pooja"}
+account2 = {"amit", "pooja", "karan"}
+
+common_followers = account1 & account2
+
+print(common_followers)
