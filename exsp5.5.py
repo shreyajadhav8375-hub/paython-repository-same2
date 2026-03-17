2e2669577424420280e4827384f92a64e7b3c728
shreyajadhav8375-hub <shreyajadhav8375@gmail.com>
2026-03-17 13:35:05 +0530

    Add files via upload

---
+++ exsp5.5.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 07:39:34 2026
+
+@author:shreya
+"""
+inventory = {"Apples": 50, "Bananas": 30}
+
+# Add new stock
+inventory["Oranges"] = 40
+
+# Update existing stock
+inventory["Apples"] += 20
+
+print(inventory)
